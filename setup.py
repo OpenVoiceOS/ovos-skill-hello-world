ab4aaff98758cbc2a07bd55f8124c0f057e8bb06
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-06-10 16:59:56 +0100

    add end2end tests (#81)
    * add end2end tests
    
    * drop blacklist tests, those belong in ovos-core
    
    * requirements.txt
    
    * build tests
    
    * fix coverage
    
    * fix coverage
    
    * m2v
    
    * refactor: packaging
    
    * refactor: packaging
    
    * refactor: packaging

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 import os
-from setuptools import setup
 from os import walk, path
+
+from setuptools import setup
 
 URL = "https://github.com/OpenVoiceOS/ovos-skill-hello-world"
 SKILL_CLAZZ = "HelloWorldSkill"  # needs to match __init__.py class name
 PYPI_NAME = "ovos-skill-hello-world"  # pip install PYPI_NAME
+SKILL_PKG = PYPI_NAME.lower().replace('-', '_')  # import name
 
-# below derived from github url to ensure standard skill_id
-SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
-SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
+SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")  # derived from github url to ensure standard skill_id
 PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 
 
 def find_resource_files():
-    resource_base_dirs = ("locale", )
-    base_dir = path.dirname(__file__)
+    resource_base_dirs = ("locale",)
+    base_dir = path.join(os.path.dirname(__file__), SKILL_PKG)
     package_data = ["*.json"]
+
     for res in resource_base_dirs:
-        if path.isdir(path.join(base_dir, res)):
-            for (directory, _, files) in walk(path.join(base_dir, res)):
+        res_dir = path.join(base_dir, res)
+        if path.isdir(res_dir):
+            for (directory, _, files) in walk(res_dir):
                 if files:
-                    package_data.append(
-                        path.join(directory.replace(base_dir, "").lstrip('/'),
-                                  '*'))
+                    relative_dir = directory.replace(base_dir + os.sep, "")
+                    package_data.append(path.join(relative_dir, "*"))
     return package_data
 
 
@@ -33,7 +34,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), "r") as 
 
 def get_version():
     """ Find the version of this skill"""
-    version_file = os.path.join(os.path.dirname(__file__), 'version.py')
+    version_file = os.path.join(os.path.dirname(__file__), SKILL_PKG, 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
@@ -58,15 +59,15 @@ def get_version():
 setup(
     name=PYPI_NAME,
     version=get_version(),
+    description='OVOS hello world skill plugin',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url=URL,
     author=SKILL_AUTHOR,
-    description='OVOS hello world skill plugin',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
-    package_dir={SKILL_PKG: ""},
-    package_data={SKILL_PKG: find_resource_files()},
     packages=[SKILL_PKG],
+    package_data={SKILL_PKG: find_resource_files()},
     include_package_data=True,
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
